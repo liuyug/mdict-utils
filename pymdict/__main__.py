1ea99043c860843f8aec7e7634df4a3d7d2a6e71
LIU Yugang <liuyug@gmail.com>
2019-03-29 14:54:38 +0800

    fix for big mdict

--- pymdict/__main__.py
+++ pymdict/__main__.py
@@ -3,11 +3,24 @@ import argparse
 
 from . import about
 from . import reader
-from .writer import pack, pack_mdd_file, pack_mdx_txt
+from .writer import pack, pack_mdd_file, pack_mdx_txt, pack_mdx_sqlite3,  \
+    txt2sqlite, sqlite2txt
+
+
+total = 0
+
+
+def make_callback(fmt):
+    def callback(value):
+        global total
+        total += value
+        print(fmt % total, end='')
+    return callback
 
 
 def run():
-    parser = argparse.ArgumentParser(prog='mdict', description=about.description)
+    epilog = 'NOTE: MDict encoding is UTF-8 and format is version 2.0'
+    parser = argparse.ArgumentParser(prog='mdict', description=about.description, epilog=epilog)
     parser.add_argument('--version', action='version',
                         version='%%(prog)s version %s - written by %s <%s>' % (
                             about.version, about.author, about.email),
@@ -17,6 +30,8 @@ def run():
     parser.add_argument('-k', dest='key', action='store_true', help='show mdx/mdd keys')
     parser.add_argument('-m', dest='meta', action='store_true', help='show mdx/mdd meta information')
     parser.add_argument('-q', dest='query', action='store_true', help='query KEY from mdx/mdd')
+    parser.add_argument('--txt-db', action='store_true', help='convert mdx txt to sqlite3 db')
+    parser.add_argument('--db-txt', action='store_true', help='convert sqlite3 db to mdx txt')
     parser.add_argument('mdict', metavar='<mdx/mdd>', help='Dictionary MDX/MDD file')
 
     group = parser.add_argument_group('Reader')
@@ -25,9 +40,12 @@ def run():
 
     group = parser.add_argument_group('Writer')
     group.add_argument('-c', dest='create', action='store_true', help='create mdx/mdd file')
+    group.add_argument('--encoding', metavar='<encoding>', default='utf-8', help='mdx txt file encoding')
     group.add_argument('resource', nargs='*', help='Dictionary resource directory/file')
 
     args = parser.parse_args()
+
+    global total
 
     if args.meta:
         meta = reader.meta(args.mdict)
@@ -45,6 +63,16 @@ def run():
             count += 1
             key = key.decode('utf-8')
             print(key)
+    elif args.txt_db:
+        total = 0
+        fmt = '\rConvert "%s": %%s' % args.mdict
+        txt2sqlite(args.mdict, callback=make_callback(fmt))
+        print()
+    elif args.db_txt:
+        total = 0
+        fmt = '\rConvert "%s": %%s' % args.mdict
+        sqlite2txt(args.mdict, callback=make_callback(fmt))
+        print()
     elif args.query:
         qq = reader.query(args.mdict, args.query)
         for q in qq:
@@ -55,21 +83,24 @@ def run():
         is_mdd = args.mdict.endswith('.mdd')
         dictionary = []
         for resource in args.resource:
-            print('Scan "%s"...' % resource)
+            fmt = '\rScan "%s": %%s' % resource
+            total = 0
             if is_mdd:
-                d = pack_mdd_file(resource)
+                d = pack_mdd_file(resource, callback=make_callback(fmt))
+            elif resource.endswith('.db'):
+                d = pack_mdx_sqlite3(resource, encoding=args.encoding, callback=make_callback(fmt))
             else:
-                d = pack_mdx_txt(resource)
-            print('\tentry:', len(d))
+                d = pack_mdx_txt(resource, encoding=args.encoding, callback=make_callback(fmt))
             dictionary.extend(d)
+        print()
         title = ''
         description = ''
         if args.title:
             title = open(args.title, 'rt').read()
         if args.description:
             description = open(args.description, 'rt').read()
-        print('Pack to "%s"' % args.create)
-        pack(args.mdict, dictionary, title, description, is_mdd=is_mdd)
+        print('Pack to "%s"' % args.mdict)
+        pack(args.mdict, dictionary, title, description, encoding=args.encoding, is_mdd=is_mdd)
     else:
         parser.print_help()
 
--- pymdict/writer.py
+++ pymdict/writer.py
@@ -1,4 +1,5 @@
 
+import sqlite3
 import struct
 import os.path
 import locale
@@ -10,10 +11,51 @@ from .base.writemdict import MDictWriter as MDictWriterBase, \
     _OffsetTableEntry as _OffsetTableEntryBase
 
 
+MDX_OBJ = {}
+
+
+def get_record_null(mdx_file, key, pos, size, encoding):
+    global MDX_OBJ
+    if mdx_file not in MDX_OBJ:
+        if mdx_file.endswith('.db'):
+            conn = sqlite3.connect(mdx_file)
+            MDX_OBJ[mdx_file] = conn
+        else:
+            f = open(mdx_file, 'rb')
+            MDX_OBJ[mdx_file] = f
+    obj = MDX_OBJ[mdx_file]
+    if mdx_file.endswith('.db'):
+        sql = 'SELECT paraphrase FROM mdx_txt WHERE entry=?'
+        c = obj.execute(sql, (key,))
+        for row in c.fetchall():    # multi entry
+            record_null = (row[0] + '\0').encode(encoding)
+            if len(record_null) == size:
+                return record_null
+    else:
+        obj.seek(pos)
+        record_null = obj.read(size - 1)
+        return record_null + b'\0'
+    return b''
+
+
 class _OffsetTableEntry(_OffsetTableEntryBase):
-    def __init__(self, key, key_null, key_len, offset, record_null, record_size):
-        super(_OffsetTableEntry, self).__init__(key, key_null, key_len, offset, record_null)
+    def __init__(self, key0, key, key_null, key_len, offset,
+                 record_pos, record_null, record_size, encoding, is_mdd):
+        super(_OffsetTableEntry, self).__init__(
+            key, key_null, key_len, offset, record_null)
+        self.key0 = key0
+        self.record_pos = record_pos
         self.record_size = record_size
+        self.encoding = encoding
+        self.is_mdd = is_mdd
+
+    def get_record_null(self):
+        if self.is_mdd:
+            return open(self.record_null, 'rb').read()
+        else:
+            return get_record_null(
+                self.record_null, self.key0,
+                self.record_pos, self.record_size, self.encoding)
 
 
 class _MdxRecordBlock(_MdxRecordBlockBase):
@@ -22,11 +64,17 @@ class _MdxRecordBlock(_MdxRecordBlockBase):
         self._compression_type = compression_type
         self._version = version
 
-    def prepare(self, is_mdd):
-        if is_mdd:
-            for t in self._offset_table:
-                t.record_null = open(t.record_null, 'rb').read()
-        super(_MdxRecordBlock, self).__init__(self._offset_table, self._compression_type, self._version)
+    def prepare(self):
+        super(_MdxRecordBlock, self).__init__(
+            self._offset_table, self._compression_type, self._version)
+
+    def clean(self):
+        if self._comp_data:
+            self._comp_data = None
+
+    @staticmethod
+    def _block_entry(t, version):
+        return t.get_record_null()
 
     @staticmethod
     def _len_block_entry(t):
@@ -46,20 +94,17 @@ class MDictWriter(MDictWriterBase):
             key_null = (key + "\0").encode(self._python_encoding)
             key_len = len(key_enc) // self._encoding_length
 
-            # set record_null to a the the value of the record. If it's
-            # an MDX file, append an extra null character.
-            if self._is_mdd:
-                record_null = record['path']
-            else:
-                record_null = (record['path'] + "\0").encode(self._python_encoding)
-                record['size'] = len(record_null)
             self._offset_table.append(_OffsetTableEntry(
+                key0=record['key'],
                 key=key_enc,
                 key_null=key_null,
                 key_len=key_len,
-                record_null=record_null,
+                record_null=record['path'],
                 record_size=record['size'],
+                record_pos=record['pos'],
                 offset=offset,
+                encoding=getattr(self, '_encoding', 'utf-8'),
+                is_mdd=self._is_mdd,
             ))
             offset += record['size']
         self._total_record_len = offset
@@ -86,11 +131,12 @@ class MDictWriter(MDictWriterBase):
         recordblocks_total_size = 0
         recordb_index = []
         for b in self._record_blocks:
-            b.prepare(self._is_mdd)
+            b.prepare()
             recordblocks_total_size += len(b.get_block())
             recordb_index.append(b.get_index_entry())
             outfile.write(b.get_block())
             callback and callback(len(b._offset_table))
+            b.clean()
         end_pos = outfile.tell()
         self._recordb_index = b''.join(recordb_index)
         self._recordb_index_size = len(self._recordb_index)
@@ -110,11 +156,11 @@ class MDictWriter(MDictWriterBase):
         self._write_record_sect(outfile, callback=callback)
 
 
-def pack(target, dictionary, title='', description='', is_mdd=False):
+def pack(target, dictionary, title='', description='', encoding='utf-8', is_mdd=False):
     def callback(value):
         bar.update(value)
 
-    writer = MDictWriter(dictionary, title=title, description=description, is_mdd=is_mdd)
+    writer = MDictWriter(dictionary, title=title, description=description, encoding=encoding, is_mdd=is_mdd)
     bar = tqdm(total=len(writer._offset_table), unit='rec')
     outfile = open(target, "wb")
     writer.write(outfile, callback=callback)
@@ -122,31 +168,109 @@ def pack(target, dictionary, title='', description='', is_mdd=False):
     bar.close()
 
 
-def pack_mdx_txt(source):
-    dictionary = []
+def txt2sqlite(source, callback=None):
+    db_name = source + '.db'
+    conn = sqlite3.connect(db_name)
+    c = conn.cursor()
+    c.execute('DROP TABLE IF EXISTS mdx_txt')
+    c.execute('CREATE TABLE mdx_txt (entry text not null, paraphrase text not null)')
+    max_batch = 1024 * 10
     with open(source, 'rt') as f:
+        count = 0
+        entries = []
         key = None
         content = []
-        for line in f.readlines():
+        for line in f:
             line = line.strip()
+            if not line:
+                continue
             if line == '</>':
                 content = ''.join(content)
-                size = len(content)
+                entries.append((key, content))
+                if count > max_batch:
+                    c.executemany('INSERT INTO mdx_txt VALUES (?,?)', entries)
+                    conn.commit()
+                    count = 0
+                    entries = []
+                key = None
+                content = []
+                callback and callback(1)
+            elif not key:
+                key = line
+                count += 1
+            else:
+                content.append(line)
+        if entries:
+            c.executemany('INSERT INTO mdx_txt VALUES (?,?)', entries)
+            conn.commit()
+        c.execute('CREATE INDEX entry_index ON mdx_txt (entry)')
+        conn.close()
+
+
+def sqlite2txt(source, callback=None):
+    mdx_txt = source + '.txt'
+    with open(mdx_txt, 'wt') as f:
+        sql = 'SELECT entry, paraphrase FROM mdx_txt'
+        with sqlite3.connect(source) as conn:
+            cur = conn.execute(sql)
+            for c in cur:
+                f.write(c[0] + '\r\n')
+                f.write(c[1] + '\r\n')
+                f.write('</>\r\n')
+                callback and callback(1)
+
+
+def pack_mdx_sqlite3(source, encoding='utf-8', callback=None):
+    dictionary = []
+    sql = 'SELECT entry, paraphrase FROM mdx_txt'
+    with sqlite3.connect(source) as conn:
+        cur = conn.execute(sql)
+        for c in cur:
+            dictionary.append({
+                'key': c[0],
+                'pos': 0,
+                'path': source,
+                'size': len((c[1] + '\0').encode(encoding)),
+            })
+            callback and callback(1)
+    return dictionary
+
+
+def pack_mdx_txt(source, encoding='utf-8', callback=None):
+    dictionary = []
+    with open(source, 'rb') as f:
+        key = None
+        content = []
+        pos = 0
+        line = f.readline()
+        while line:
+            line = line.strip()
+            if not line:
+                line = f.readline()
+                continue
+            if line == b'</>':
+                content = b''.join(content)
+                size = len(content + b'\0')
                 dictionary.append({
-                    'key': key,
-                    'path': content,
+                    'key': key.decode(encoding),
+                    'pos': pos,
+                    'path': source,
                     'size': size,
                 })
                 key = None
                 content = []
+                callback and callback(1)
             elif not key:
                 key = line
+                pos = f.tell()
             else:
                 content.append(line)
+
+            line = f.readline()
     return dictionary
 
 
-def pack_mdd_file(source):
+def pack_mdd_file(source, callback=None):
     dictionary = []
     source = os.path.abspath(source)
     if os.path.isfile(source):
@@ -156,6 +280,7 @@ def pack_mdd_file(source):
             key = key.replace(os.sep, '\\')
         dictionary.append({
             'key': key,
+            'pos': 0,
             'path': source,
             'size': size,
         })
@@ -170,7 +295,9 @@ def pack_mdd_file(source):
                     key = key.replace(os.sep, '\\')
                 dictionary.append({
                     'key': key,
+                    'pos': 0,
                     'path': fpath,
                     'size': size,
                 })
+                callback and callback(1)
     return dictionary
