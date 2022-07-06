b4205e88f7aa97cebe261ad7cbb2674d05128808
Yugang LIU <liuyug@gmail.com>
2022-07-06 14:46:27 +0800

    fix

--- py2exe.cmd
+++ py2exe.cmd
@@ -1,6 +1,29 @@
 
+setlocal
 rmdir build /s /q
 rmdir dist\mdict_utils /s /q
-pyinstaller mdict_utils.spec
+
+mkdir build
+
+set script=build\script.py
+echo import mdict_utils.__main__ >> %script%
+echo mdict_utils.__main__.run() >> %script%
+
+type %script%
+
+call ..\env_noqt\Scripts\activate
+pyinstaller ^
+--name mdict ^
+--icon logo.ico ^
+--noconfirm ^
+--onefile ^
+--console ^
+%script%
+
+call ..\env_noqt\Scripts\deactivate
+
+del *.spec
+
+endlocal
 
 dist\mdict.exe --version
