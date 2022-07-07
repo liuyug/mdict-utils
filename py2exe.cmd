cce96bc988beca101d4de2dbdd9d91b6bf922af1
Yugang LIU <liuyug@gmail.com>
2022-07-07 08:50:34 +0800

    use random key

--- py2exe.cmd
+++ py2exe.cmd
@@ -9,12 +9,17 @@ set script=build\script.py
 echo import mdict_utils.__main__ >> %script%
 echo mdict_utils.__main__.run() >> %script%
 
+for /F %%i in ('python -c "import random, string; print(''.join(random.choices(string.digits + string.ascii_letters, k=16)))"') do ( set key=%%i)
+
 type %script%
+echo %key%
 
 call ..\env_noqt\Scripts\activate
 pyinstaller ^
+--key %key% ^
 --name mdict ^
 --icon logo.ico ^
+--exclude-module tkinter ^
 --noconfirm ^
 --onefile ^
 --console ^
