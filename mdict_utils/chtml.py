85c04d15d17687d359516c6d4b4a5d9144bd6238
Yugang LIU <liuyug@gmail.com>
2024-07-06 20:02:17 +0800

    fix convert-html bug

--- mdict_utils/chtml.py
+++ mdict_utils/chtml.py
@@ -27,15 +27,19 @@ class CompactHTML():
         self._css = css
 
     def to_html(self, chtml):
-        html = []
         re_index = re.compile(b'`(\\d+)`')
-        pos = 0
-        last_end_tag = b''
-        for m in re_index.finditer(chtml):
-            html.append(chtml[pos:m.start()])
+        index_iter = re_index.finditer(chtml)
+        if index_iter:
+            html = []
+            pos = 0
+            last_end_tag = b''
+            for m in index_iter:
+                html.append(chtml[pos:m.start()])
+                html.append(last_end_tag)
+                html.append(self._css[m.group(1)][0])
+                last_end_tag = self._css[m.group(1)][1]
+                pos = m.end()
             html.append(last_end_tag)
-            html.append(self._css[m.group(1)][0])
-            last_end_tag = self._css[m.group(1)][1]
-            pos = m.end()
-        html.append(last_end_tag)
-        return b''.join(html)
+            return b''.join(html)
+        else:
+            return chtml
